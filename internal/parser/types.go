c114efa234bb59bf189b0a74110407a0b8587639
alex <alex.oblat.lv>
2025-05-31 15:17:26 +0300

    Some changes

---
+++ internal/parser/types.go
@@ -0,0 +1,26 @@
+package parser
+
+import (
+	"github.com/akyaiy/gosally-basenode/internal/config"
+	"github.com/akyaiy/gosally-basenode/internal/logger"
+)
+
+type ParserD interface {
+	ParseArgs(args []string) error
+	ParseCMDlineArgs() error
+
+	ParseConfigFile(file string) error
+
+	ParseEnvVars() error
+
+	SetConfigValue(_key string, _val string) error
+	setValue(key, value string, source config.Source) error
+
+	parseShortParameters(args string, source config.Source) error
+	parseLongParameter(arg string, source config.Source) error
+}
+
+type Parser struct {
+	Log logger.Log
+	config.Config
+}
