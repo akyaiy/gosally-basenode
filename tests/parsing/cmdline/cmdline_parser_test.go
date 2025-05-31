2c6eb4bfe17eb37cd34bf76e9bee52f6fea4afa2
alex <alex.oblat.lv>
2025-05-31 15:17:49 +0300

    Add unit test for cmdline parsing

---
+++ tests/parsing/cmdline/cmdline_parser_test.go
@@ -0,0 +1,55 @@
+package cmdline_test
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/akyaiy/gosally-basenode/internal/config"
+	"github.com/akyaiy/gosally-basenode/internal/parser"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestParseArgs(t *testing.T) {
+	args := []string{"prog", "--listen-address=0.0.0.0"}
+
+	var p = &parser.Parser{Config: config.Config{}}
+	err := p.ParseArgs(args)
+	if err != nil {
+		t.Errorf("ParseArgs failed: %v", err)
+	}
+	want := config.TypeConfig{
+		"exec-name":      config.ConfValue{Value: "prog", Source: config.SrcCMD},
+		"listen-address": config.ConfValue{Value: "0.0.0.0", Source: config.SrcCMD},
+	}
+	if !reflect.DeepEqual(want, p.Config.Vars) {
+		t.Fatalf("Expected %+v, got %+v", want, p.Config.Vars)
+	}
+}
+
+/*
+
+func TestParseArgs(t *testing.T) {
+	args := []string{"prog", "--listen-address=0.0.0.0"}
+	var p config.ConfigD = &parser.Parser{
+		Log:    logger.InitBaseLog(),
+		Config: config.TypeConfig,
+	}
+	err := p.ParseArgs(args)
+	if err != nil {
+		t.Errorf("ParseArgs failed: %v", err)
+	}
+	want := config.TypeConfig{
+		"exec-name":      config.ConfValue{Value: "prog", Source: config.SrcCMD},
+		"listen-address": config.ConfValue{Value: "0.0.0.0", Source: config.SrcCMD},
+	}
+
+	if !reflect.DeepEqual(want, p.ProgramConfig()) {
+		t.Fatalf("Expected %+v, got %+v", want, p.ProgramConfig())
+	}
+}
+*/
